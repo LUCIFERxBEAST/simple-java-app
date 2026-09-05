e93183aa6426891ae62cb6bbf5cb63732a327154
beast <LUCIFERxBEAST>
2026-09-05 11:59:55 +0530

    Initital Java CI/CD commit

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App{
+    public static void main(String[] args){
+        System.out.println("Java CI/CD Application started...");
+        while (true) {
+            Thread.sleep(5000);
+            System.out.println("Hello from Java CI/CD Pipeline!");
+        }
+    }
+}
